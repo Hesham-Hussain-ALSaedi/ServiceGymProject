c62f5816f0346c09d5eaaa37678dd658cefa4d1e
Hesham-Hussain-ALSaedi <TU4201336@taibahu.edu.sa>
2023-12-02 20:37:29 +0300

    go

---
+++ script.js
@@ -0,0 +1,8 @@
+
+const bookButton = document.getElementById("bookButton");
+const trainingClasses = document.getElementById("classSchulde");
+
+trainingClasses.addEventListener("click", function(){
+
+  trainingClasses.style.display ="none";
+});
